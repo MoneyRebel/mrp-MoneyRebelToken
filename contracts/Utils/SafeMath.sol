7f5f09350992664278fbf16736c70c4f5fd9c472
doktorBabo <rok.babic@gmail.com>
2017-12-04 15:12:00 +0100

    Downgrade because "pure" fuckup in solidity

--- contracts/Utils/SafeMath.sol
+++ contracts/Utils/SafeMath.sol
@@ -1,20 +1,20 @@
-pragma solidity ^0.4.13;
+pragma solidity ^0.4.14;
 
 contract SafeMath {
     
     uint256 constant MAX_UINT256 = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
 
-    function safeAdd(uint256 x, uint256 y) pure internal returns (uint256 z) {
+    function safeAdd(uint256 x, uint256 y) constant internal returns (uint256 z) {
         require(x <= MAX_UINT256 - y);
         return x + y;
     }
 
-    function safeSub(uint256 x, uint256 y) pure internal returns (uint256 z) {
+    function safeSub(uint256 x, uint256 y) constant internal returns (uint256 z) {
         require(x >= y);
         return x - y;
     }
 
-    function safeMul(uint256 x, uint256 y) pure internal returns (uint256 z) {
+    function safeMul(uint256 x, uint256 y) constant internal returns (uint256 z) {
         if (y == 0) {
             return 0;
         }
